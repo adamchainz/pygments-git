279c86c8d58312e77e8ae954a5925c071d5e08ed
Adam Johnson <me@adamj.eu>
2023-04-04 19:00:13 +0100

    Add git-rebase-todo lexer

--- tests/test_pygments_git.py
+++ tests/test_pygments_git.py
@@ -72,7 +72,7 @@ def golden_file(request):
             "</thead>",
             "<tbody>",
         ]
-        for testid, (lexer, given, result) in checker.cases.items():
+        for testid, (lexer, given, result) in sorted(checker.cases.items()):
             lines.extend(
                 [
                     "<tr>",
@@ -149,5 +149,47 @@ def test_git_console(golden_file):
 
         $ git commit -m "Spin it"
         [main 0bcdb8f] Spin it
+        """,
+    )
+
+
+def test_git_rebase_todo(golden_file):
+    golden_file.check(
+        "test_git_rebase_todo",
+        "git-rebase-todo",
+        """\
+        pick d05ab26 Flick it
+        update-ref refs/heads/other
+
+        # Rebase 7d2c29b..d05ab26 onto 7d2c29b (1 command)
+        #
+        # Commands:
+        # p, pick <commit> = use commit
+        # r, reword <commit> = use commit, but edit the commit message
+        # e, edit <commit> = use commit, but stop for amending
+        # s, squash <commit> = use commit, but meld into previous commit
+        # f, fixup [-C | -c] <commit> = like "squash" but keep only the previous
+        #                    commit's log message, unless -C is used, in which case
+        #                    keep only this commit's message; -c is same as -C but
+        #                    opens the editor
+        # x, exec <command> = run command (the rest of the line) using shell
+        # b, break = stop here (continue rebase later with 'git rebase --continue')
+        # d, drop <commit> = remove commit
+        # l, label <label> = label current HEAD with a name
+        # t, reset <label> = reset HEAD to a label
+        # m, merge [-C <commit> | -c <commit>] <label> [# <oneline>]
+        #         create a merge commit using the original merge commit's
+        #         message (or the oneline, if no original merge commit was
+        #         specified); use -c <commit> to reword the commit message
+        # u, update-ref <ref> = track a placeholder for the <ref> to be updated
+        #                       to this position in the new commits. The <ref> is
+        #                       updated at the end of the rebase
+        #
+        # These lines can be re-ordered; they are executed from top to bottom.
+        #
+        # If you remove a line here THAT COMMIT WILL BE LOST.
+        #
+        # However, if you remove everything, the rebase will be aborted.
+        #
         """,
     )

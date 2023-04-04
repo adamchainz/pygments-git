60618cd460ed96cc3340c704e4d4fabb3b702cef
Adam Johnson <me@adamj.eu>
2023-04-04 09:02:41 +0100

    Finish

--- tests/test_pygments_git.py
+++ tests/test_pygments_git.py
@@ -1,47 +1,59 @@
 from __future__ import annotations
 
 from html import escape as e
-from html.parser import HTMLParser
 from pathlib import Path
 from textwrap import dedent
 
+from bs4 import BeautifulSoup
 import pytest
 from pygments import highlight
 from pygments.lexers import get_lexer_by_name
 from pygments.formatters import HtmlFormatter
 
 MODULE_DIR = Path(__file__).parent.resolve()
-formatter = HtmlFormatter()
+formatter = HtmlFormatter(style="colorful")
 
 
 @pytest.fixture(scope="module")
 def golden_file(request):
+    save = request.config.getoption("--save")
+
     index = MODULE_DIR / "index.html"
 
-    parser = TestTableParser()
-    parser.feed(index.read_text())
-
-    print(parser.rows)
-    1 / 0
+    text = index.read_text()
+    soup = BeautifulSoup(text, "html.parser")
+    loaded_cases: dict[str, tuple[str, str, str]] = {}
+    for row in soup.find("tbody").find_all("tr"):
+        tds = iter(row.find_all("td"))
+        testid = next(tds).get_text().strip()
+        lexer = next(tds).get_text().strip()
+        given = next(tds).get_text().strip()
+        result = str(next(tds).find("div", {"class": "highlight"})).strip()
+        loaded_cases[testid] = (lexer, given, result)
 
     class Checker:
         def __init__(self):
-            self.cases: list[tuple[str, str, str, str]] = []
+            self.cases: dict[str, tuple[str, str, str]] = {}
 
         def check(self, testid: str, lexer: str, given: str):
-            given = dedent(given)
+            given = dedent(given).rstrip("\n")
             result = highlight(
                 given,
                 lexer=get_lexer_by_name(lexer, stripnl=False),
                 formatter=formatter,
             )
-            self.cases.append((lexer, testid, given, result))
+            result = str(BeautifulSoup(result, "html.parser")).strip()
+            self.cases[testid] = (lexer, given, result)
+            if not save and testid in loaded_cases:
+                loaded_lexer, loaded_given, loaded_result = loaded_cases[testid]
+                if lexer == loaded_lexer and given == loaded_given:
+                    assert result == loaded_result
 
     checker = Checker()
 
     yield checker
 
-    if not request.config.getoption("--save"):
+    if not save:
         return
 
     lines = [
@@ -62,7 +74,7 @@ def golden_file(request):
         "</thead>",
         "<tbody>",
     ]
-    for lexer, testid, given, result in checker.cases:
+    for testid, (lexer, given, result) in checker.cases.items():
         lines.extend(
             [
                 "<tr>",
@@ -80,11 +92,7 @@ def golden_file(request):
                 "</code>",
                 "</td>",
                 "<td>",
-                "<code class=highlight>",
-                "<pre>",
                 result,
-                "</pre>",
-                "</code>",
                 "</td>",
                 "</tr>",
             ]
@@ -101,39 +109,6 @@ def golden_file(request):
     index.write_text("\n".join(lines))
 
 
-class TestTableParser(HTMLParser):
-    def __init__(self):
-        super().__init__()
-        self.in_table = False
-        self.in_row = False
-        self.seen_thead = False
-        self.current_row = []
-        self.rows = []
-
-    def handle_starttag(self, tag, attrs):
-        if tag == "table":
-            self.in_table = True
-        elif self.in_table and self.seen_thead and tag == "tr":
-            self.in_row = True
-            self.current_row = []
-
-    def handle_data(self, data):
-        if self.in_row:
-            data = data.strip()
-            if data:
-                self.current_row.append(data)
-
-    def handle_endtag(self, tag):
-        if self.in_table and tag == "thead":
-            self.seen_thead = True
-        if self.in_table and tag == "tr":
-            self.in_row = False
-            if self.current_row:
-                self.rows.append(tuple(self.current_row))
-        elif tag == "table":
-            self.in_table = False
-
-
 def test_git_console(golden_file):
     golden_file.check(
         "test_git_console",
@@ -147,19 +122,3 @@ def test_git_console(golden_file):
         414a4ce Twist it
         """,
     )
-
-
-#     session =
-
-#     result = highlight_html("git-console", session)
-
-#     assert result == (
-#         '<div class="highlight"><pre>'
-#         "<span></span>"
-#         '<span class="gp">$ </span>git<span class="w"> </span>reflog\n'
-#         '<span class="mh">819b03d</span><span class="go"> (HEAD -&gt; main, origin/main) HEAD@{0}: commit: Bop it</span>\n'
-#         '<span class="mh">414a4ce</span> <span class="nt">HEAD@{1}:</span><span class="go"> commit (amend): Twist it</span>\n'
-#         '<span class="gp">$ </span>git<span class="w"> </span>log<span class="w"> </span>--oneline<span class="w"> </span><span class="s1">&#39;main&#39;</span>\n'
-#         '<span class="mh">819b03d</span><span class="go"> Bop it</span>\n'
-#         '<span class="mh">414a4ce</span><span class="go"> Twist it</span>\n</pre></div>\n'
-#     )
